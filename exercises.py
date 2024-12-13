8d4466309acdb3cc67a26c89555408f421649530
chamer79 <chamer79@gmail.com>
2024-12-12 21:04:04 -0500

    completed exercise 3

--- exercises.py
+++ exercises.py
@@ -28,4 +28,24 @@ def simple_interest(princ, rate, interest):
     return (princ * rate * interest) / 100
 
 
-print('Exercise 2:', simple_interest(1000, 5, 2))
+# print('Exercise 2:', simple_interest(1000, 5, 2))
+
+
+# Exercise 3: Apply a Discount
+#
+# Write a function named `apply_discount` that takes a product's price and a discount percentage (from 0 to 100).
+# Return the new price after applying the discount.
+#
+# Examples:
+# apply_discount(100, 25) should return 75.
+# apply_discount(80, 10) should return 72.
+#
+# Define your function and call it to display the discounted price.
+def apply_discount(price, discount):
+    if discount >=0 and discount <=100:
+        return price - (price * (discount / 100))
+    else:
+        return(f"{discount}% discount is invalid. Enter a valid discount from 0 - 100.")
+
+
+print('Exercise 3:', apply_discount(100, 25))
