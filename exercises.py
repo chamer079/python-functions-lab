24cccb683333d43def909df15a9730f144c6c5cb
chamer79 <chamer79@gmail.com>
2024-12-15 18:51:28 -0500

    completed exercise 6

--- exercises.py
+++ exercises.py
@@ -84,12 +84,31 @@ def convert_temperature(temp, temp_scale):
 def sum_to(n):
     return sum(range(n + 1))
 
-print('Exercise 5:', sum_to(6))
+# print('Exercise 5:', sum_to(6))
 
 """
 return sum(range(n + 1))
 
 sum() -> adds all items in a tuple
 range() -> returns a sequence of numbers starting from 0 - increments by 1 (default) - stops before the specified number
-
 """
+
+
+# Exercise 6: Find the Largest Number
+#
+# Write a function named `largest` that takes three integers as arguments and returns the largest of them.
+#
+# Examples:
+# largest(1, 2, 3) should return 3.
+# largest(10, 4, 2) should return 10.
+#
+# Define your function and test it with different inputs.
+def largest(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        return num1
+    elif num2 > num3:
+        return num2
+    else:
+        return num3
+
+print('Exercise 6:', largest(1, 2, 3))
